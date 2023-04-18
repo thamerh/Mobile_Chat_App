8d67e25898f33d4da2f0346073cafb2e1004a2a7
thamerh <hamdithamer99@gmail.com>
2023-04-18 20:35:44 +0200

    feat: create profile components for list of chat user profiles

---
+++ frontend/src/components/Profiles.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {View,Text,StyleSheet,Image,TouchableOpacity} from 'react-native';
+
+
+const Profile = ({username,uri,onPress}) => {    
+    return(
+       
+        <TouchableOpacity onPress={onPress} >
+         <View style={styles.container}>
+            <Image source={{uri:uri}} style={styles.avatarStyle}/>
+            <Text style={styles.nameStyle}>{username}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+}
+export default Profile;
+const styles = StyleSheet.create({
+    container:{
+        alignItems:'center',
+        marginTop:20,
+        marginRight:17
+    },
+    avatarStyle:{
+        width:60,
+        height:60,
+        borderRadius:30
+    },
+    nameStyle:{
+        marginTop:10,
+        fontSize:11,
+        color:'#fff',
+        fontFamily:'Montserrat_700Bold',
+        justifyContent: 'center',
+    }
+})
