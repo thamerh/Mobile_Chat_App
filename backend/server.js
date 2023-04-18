a8be8bb3f2c21821e910097919350858d61e6a3c
thamerh <hamdithamer99@gmail.com>
2023-04-18 23:57:36 +0200

    feat: create instance of the Socket.IO Server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ import db from "./config/Database.js";
 import router from "./routes/routes.js";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import http from 'http';
+import { Server } from 'socket.io';
+
 
 const app = express();
 dotenv.config();
@@ -25,6 +28,31 @@ app.use(router);
 app.get('/', (req, res) => {
   res.json({ success: true, message: "welcome to backend zone"});
 });
-const port=process.env.PORT || 8000
 
-app.listen(port,console.log(`server started in port ${port}`));
+const server = http.createServer(app);
+const io = new Server(server);
+// Export the 'io' instance
+export { io };
+io.on("connection", (socket) => {
+  console.log("A user connected!");
+
+  // socket.on("joinRoom", (roomId) => {
+  //   console.log(`user joined room ${roomId}`);
+  //   socket.join(roomId);
+  // });
+
+  // socket.on("message", async (message) => {
+  //   console.log(` all message: ${message}`);
+  //   const roomId = message.chatRoomId;
+  //   await ChatRoom.findAll();
+  //   io.to(roomId).emit("AllMessage", message); // emit a new message event to all clients in the room
+  // });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected!");
+  });
+});
+
+
+const port=process.env.PORT || 8000
+server.listen(port, () => console.log(`Server started in port ${port}`));
